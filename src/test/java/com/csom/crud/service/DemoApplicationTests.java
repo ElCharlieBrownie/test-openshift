f5fbeda28b057b5bedf55016d47b0560c2392756
Carlos Soriano <carlos.soriano.dev@gmail.com>
2022-02-28 15:04:04 -0600

    config jacoco

--- src/test/java/com/csom/crud/service/DemoApplicationTests.java
+++ src/test/java/com/csom/crud/service/DemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.csom.crud.service;
 
+import static org.junit.jupiter.api.Assertions.assertNull;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,8 @@ class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		ControllerCrud crl = new ControllerCrud();
+		assertNull(crl, "valor");
 	}
 
 }
